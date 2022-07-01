f2d1ce725dad9ceb95bdf2018e3886a9f9604584
Bietham <a.osokin.sas@gmail.com>
2022-07-01 22:36:37 +0500

    Task 1 Commit

---
+++ task 1/Program/Program.cs
@@ -0,0 +1,115 @@
+﻿using geoCalc;
+using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region circle tests
+
+            Console.WriteLine($"-------Circle Static Tests-------\n" );
+
+            //Normal calculation
+            Console.WriteLine($"Area: {Circle.Area(3)}\n");
+
+            //Argument exception
+            try
+            {
+                Console.WriteLine($"Area exception variable: {Circle.Area(-1)}\n");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"{ae.Message}\n");
+            }
+
+
+            Console.WriteLine($"-------Circle Non-static Tests-------\n");
+
+            var a = new Circle(2);
+
+            //Normal Calculation
+
+            Console.WriteLine($"Area: {a.Area()}\n");
+
+            //Argument Exception
+            try
+            {
+                var a1 = new Circle(-1);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Invalid variable exception in constructor: {ae.Message}\n");
+            }
+            #endregion
+
+
+            #region triangle tests
+
+            Console.WriteLine($"-------Triangle Static Tests-------\n");
+
+            //Normal Area Test
+            Console.WriteLine($"Area: {Triangle.Area(2, 3, 4)}\n");
+
+            try
+            {
+                Console.WriteLine($"{Triangle.Area(-1, 3, 0)}\n");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Area exception, incorrect side value: {ae.Message}\n");
+            }
+
+            try
+            {
+                Console.WriteLine($"{Triangle.Area(1, 4, 2)}\n");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Area exception, incorrect sides: {ae.Message}\n");
+            }
+            #endregion
+
+            Console.WriteLine($"Is Triangle Right (3, 4, 5)?: {Triangle.isRight(3, 4, 5)}\n");
+
+            Console.WriteLine($"Is Triangle Right? (4, 4, 5): {Triangle.isRight(4, 4, 5)}\n");
+
+            Console.WriteLine($"-------Triangle Non-static Tests-------\n");
+                
+            var b = new Triangle(3, 4, 5);
+            var c = new Triangle(2, 3, 4);
+
+            //Normal Area calculation
+
+            Console.WriteLine($"Area: {a.Area()}\n");
+
+            Console.WriteLine($"Is triangle Right? (3, 4, 5): {b.isRight()}\n");
+
+            Console.WriteLine($"Is triangle Right? (2, 3, 4): {c.isRight()}\n");
+
+            //Exceptions
+
+            try
+            {
+                var b1 = new Triangle(-1, 2, 3);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Area exception, incorrect side value: {ae.Message}\n");
+            }
+
+            try
+            {
+                var b1 = new Triangle(1, 4, 1);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"Area exception, incorrect sides: {ae.Message}\n");
+            }
+
+
+        }
+    }
+}
+
---
+++ task 1/geoCalc/geoCalc.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace geoCalc
+{
+
+    public interface Figure
+    {
+        public double Area();
+
+
+
+    }
+    public class Circle : Figure
+    {
+        public double _r { get; set; }
+
+        public Circle (double r)
+        {
+            _r = r;
+
+            if (_r <= 0)
+            {
+                throw new ArgumentException("Radius of a circle must be greater than zero");
+            }
+        }
+
+        public double Area()
+        {
+            return (Math.PI * Math.Pow(_r, 2));
+        }
+
+        public static double Area(double r)
+        {
+            if (r <= 0)
+            {
+                throw new ArgumentException("Radius of a circle must be greater than zero");
+            }
+            return (Math.PI * Math.Pow(r, 2));
+
+        }
+    }
+
+    public class Triangle : Figure
+    {
+        public double _a { get; set; }
+        public double _b { get; set; }
+        public double _c { get; set; }
+
+        public Triangle (double a, double b, double c)
+        {
+            if(a <=0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Side of the triangle must be greater then zero.");
+            }
+
+            if(a >= b+c || b >= a+c || c >= a + b)
+            {
+                throw new ArgumentException("Invalid triangle parameters. Sum of two sides can not be less then length of the other side.");
+            }
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public double Semiperimeter()
+        {
+            return (_a + _b + _c) / 2;
+        }
+
+        public double Area()
+        {
+            return Math.Sqrt(Semiperimeter() * (Semiperimeter() - _a) * (Semiperimeter() - _b) * (Semiperimeter() - _c));
+        }
+
+        public bool isRight()
+        {
+            var sides = new List<double> { _a, _b, _c };
+
+            double greaterSide = sides.Max();
+
+            sides.Remove(greaterSide);
+
+            if (Math.Pow(greaterSide, 2) == (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2)))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static double  Area (double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Side of the triangle must be greater then zero.");
+            }
+
+            if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                throw new ArgumentException("Invalid triangle parameters. Sum of two sides can not be less then length of the other side.");
+            }
+
+            var Semiperimeter = (a + b + c) / 2;
+
+            return Math.Sqrt(Semiperimeter * (Semiperimeter - a) * (Semiperimeter - b) * (Semiperimeter - c));
+        }
+
+        public static bool isRight(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Side of the triangle must be greater then zero.");
+            }
+
+            if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                throw new ArgumentException("Invalid triangle parameters. Sum of two sides can not be less then length of the other side.");
+            }
+
+
+            var sides = new List<double> { a, b, c };
+
+            double greaterSide = sides.Max();
+
+            sides.Remove(greaterSide);
+
+            if (Math.Pow(greaterSide, 2) == (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+     
+}
